bd4ae430d81b224e5e371f701d570efed8a89c4a
Foat Akhmadeev <foat.akhmadeev@gmail.com>
2014-03-12 23:32:58 +0400

    buf eq

--- quiz3/histeq2.m
+++ quiz3/histeq2.m
@@ -8,8 +8,15 @@ h = imhist(img);
 pn = h / total;
 
 outimg = zeros(s);
+
+l1 = length(h);
+buf = zeros(l1);
+for i=1:l1
+    buf(i) = sum(pn(1:i));
+end
+
 for i=1:total
-    outimg(i) = floor(255 * sum(pn(1:img(i))));
+    outimg(i) = floor(255 * buf(img(i)));
 end
 
 end
